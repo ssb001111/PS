98a68a2f5fdd7aa79ca26d566b35c8444b22cad4
Soo Bin Shin <81582079+ssb001111@users.noreply.github.com>
2024-07-11 09:41:17 +0900

    20365

--- Baekjoon/Greedy/20365.cpp
+++ Baekjoon/Greedy/20365.cpp
@@ -7,47 +7,46 @@ using namespace std;
 int main() {
 
 	cin.tie(NULL);
-  ios_base::sync_with_stdio(false);
-  
-  int n;
-  cin >> n;
+    ios_base::sync_with_stdio(false);
+    
+    int n;
+    cin >> n;
 
-  int blue = 0;
-  int red = 0;
+    int blue = 0;
+    int red = 0;
 
-  int prev_blue = 0;
-  int prev_red = 0;
+    int prev_blue = 0;
+    int prev_red = 0;
 
-  string str;
-  cin >> str;
+    string str;
+    cin >> str;
 
-  for (int i = 0; i < n; i++){
-      if (str[i] == 'B'){
-          if (prev_blue == 0){
-              prev_red = 0;
-              prev_blue = 1;
-              blue += 1;
-          }
-      }
-      else{
-          if (prev_red == 0){
-              prev_blue = 0;
-              prev_red = 1;
-              red += 1;
-          }
-      }
-  }
+    for (int i = 0; i < n; i++){
+        if (str[i] == 'B'){
+            if (prev_blue == 0){
+                prev_red = 0;
+                prev_blue = 1;
+                blue += 1;
+            }
+        }
+        else{
+            if (prev_red == 0){
+                prev_blue = 0;
+                prev_red = 1;
+                red += 1;
+            }
+        }
+    }
 
-  int result = 0;
-  if (blue <= red){
-      result = blue + 1;
-  }
-  else{
-      result = red + 1;
-  }
+    int result = 0;
+    if (blue <= red){
+        result = blue + 1;
+    }
+    else{
+        result = red + 1;
+    }
 
-  cout << result;
-      
-  return 0;
-
+    cout << result;
+        
+    return 0;
 }
