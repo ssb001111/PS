47de357796824c5e622b0ee50c50e36cd038b6e6
Soo Bin Shin <81582079+ssb001111@users.noreply.github.com>
2024-07-17 01:53:48 +0900

    11727

---
+++ Baekjoon/DP1/11727.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+vector<int> v;
+
+int func(int i){
+  if (v[i] == -1){
+      int a = func(i-1) + 2 * func(i-2);
+      v[i] = a % 10007;
+      return v[i];
+  }
+  else{
+      return v[i];
+  }
+}
+
+int main() {
+
+  cin.tie(NULL);
+  ios_base::sync_with_stdio(false);
+
+  int n;
+  cin >> n;
+
+  if (n == 1){
+      cout << 1;
+  }
+  else if (n == 2){
+      cout << 3;
+  }
+  else{
+      v = vector<int>(n);
+      for (int i = 0; i < n; i++){
+          v[i] = -1;
+      }
+      v[1] = 1;
+      v[2] = 3;
+
+      int ans = func(n-1) + 2 * func(n-2);
+      int s = 10007;
+
+      cout << ans % s;
+  }
+
+  return 0;
+}
