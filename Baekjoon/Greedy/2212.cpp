b8a3901ce429ad5d8101a07067d983e0203205a9
Soo Bin Shin <81582079+ssb001111@users.noreply.github.com>
2024-08-02 03:04:53 +0900

    2212

---
+++ Baekjoon/Greedy/2212.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    
+    int n, k;
+    cin >> n >> k;
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    stable_sort(arr, arr+n);
+
+    int arr_v2[n-1];
+    for (int i = 0; i < n-1; i++){
+        arr_v2[i] = arr[i+1] - arr[i];
+    }
+    stable_sort(arr_v2, arr_v2+n-1, greater<int>());
+
+    int result = 0;
+
+    for (int i = k-1; i < n-1; i++){
+        result += arr_v2[i];
+    }
+
+    cout << result;
+
+    return 0;
+}
